2db6290fbd0849a086c0a375ffde6d378e7546e1
Ying-yIng-yiNg-yinG <cya.chen.ya@gmail.com>
2026-07-27 15:35:12 +0800

    Update early-init.el

--- config/emacs/early-init.el
+++ config/emacs/early-init.el
@@ -7,6 +7,7 @@
 (setq gc-cons-percentage 1.0)
 (setq gc-cons-threshold most-positive-fixnum)
 (setq read-process-output-max #x10000)
+(setq native-comp-async-report-warnings-errors 'silent)
 (let ((default-file-name-handler-alist file-name-handler-alist)
       (default-load-suffixes load-suffixes)
       (default-load-file-rep-suffixes load-file-rep-suffixes))
@@ -19,10 +20,13 @@
                     load-file-rep-suffixes default-load-file-rep-suffixes
                     file-name-handler-alist default-file-name-handler-alist))
             101))
+(setq-default initial-scratch-message
+              (concat ";; Happy hacking, " user-login-name " - Emacs ♥ you!\n\n"))
 (when (boundp 'load-path-filter-function)
   (setq load-path-filter-function #'load-path-filter-cache-directory-files))
-(setq load-prefer-newer noninteractive)
-(prefer-coding-system 'utf-8)
+(prefer-coding-system 'utf-8-unix)
+(setq-default buffer-file-coding-system 'utf-8-unix)
+(setq default-process-coding-system '(utf-8-unix . utf-8-unix))
 (setq use-package-enable-imenu-support t)
 (setq inhibit-startup-message t)
 (setq inhibit-startup-screen t)
@@ -31,7 +35,7 @@
 (setq default-frame-alist '((menu-bar-lines . 0)
                             (tool-bar-lines . 0)
                             (internal-border-width . 12)
-                            (horizontal-scroll-bars)
-                            (vertical-scroll-bars)))
+                            (horizontal-scroll-bars . nil)
+                            (vertical-scroll-bars . nil)))
 
 ;;; early-init.el ends here
