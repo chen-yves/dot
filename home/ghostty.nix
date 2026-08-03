4016936beec3e68c8c192434ed22c479424fdc21
chen-yves <cya.chen.ya@gmail.com>
2026-08-03 14:46:44 +0800

    Delete home/ghostty.nix

--- home/ghostty.nix
+++
@@ -1,8 +0,0 @@
-{ config, pkgs, ... }:
-{
-  home.packages = with pkgs; [
-    ghostty-bin
-  ];
-  xdg.configFile."ghostty/config".source =
-    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dot/config/ghostty/config";
-}
